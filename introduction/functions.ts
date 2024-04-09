3e22660b627f2e6d119badb334938a12351aab1c
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-04-09 17:27:25 +0530

    Refactor functions.js and functions.ts files

---
+++ introduction/functions.ts
@@ -0,0 +1,26 @@
+function hello(num) {
+    return num * 2;
+}
+hello(2);
+function Name(name) {
+    return name;
+}
+Name("Lokesh");
+function isTrue(value) {
+    return value;
+}
+isTrue(true);
+function signup(name, age, location, email) {
+    return name + age;
+}
+signup("Lokesh", 25, "Hyderabad", "lokesh@gmail.com");
+// add numbers
+function add(a, b) {
+    return a + b;
+}
+add(2, 2);
+//part
+function part(a, b) {
+    // return "a-b" // strict call
+    return a - b;
+}
