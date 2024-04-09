ea5b455937d408a584287cc5f78deb376779f69b
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-04-09 17:10:05 +0530

    "Intro to Typescript Learnt about " Types and Functions how they are used ""

---
+++ introduction/functions.js
@@ -0,0 +1,8 @@
+function hello(num) {
+    return num * 2;
+}
+hello(2);
+function Name(name) {
+    return name;
+}
+Name("Lokesh");
---
+++ introduction/intro.ts
@@ -0,0 +1,13 @@
+// let name = 'John';// declared
+// let age = 30;//declared
+// let location = 'New York'; //it wont work declared
+
+
+
+let obj = {
+    name: 'John',
+    age: 30,
+    location: 'New York'
+};
+
+console.log(obj.name);
